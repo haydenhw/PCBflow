4f26f9dfe5d5733a657cd0d87f83748cc3a3da54
hayden7913 <hayden321@gmail.com>
2017-12-08 10:42:11 -0800

    added createTempElement function

--- client/src/helpers/generateThumbnail (lula's conflicted copy 2017-12-08).js
+++ client/src/helpers/generateThumbnail (lula's conflicted copy 2017-12-08).js
@@ -1,27 +1,14 @@
 import Konva from 'konva';
 
-function createTempElement(elementId) {
-  return () => {
-    const tempElement = document.createElement('div');
-    tempElement.id = elementId;
-    document.body.appendChild(tempElement);
-
-    return tempElement;
-  };
-}
-
-const createTempContainer = createTempElement('container');
-
 export function cropStage(boardLayer) {
+  console.log('one')
   const boardClone = boardLayer.clone();
   const boardCloneAttrs = boardClone.get('.boardGroup')[0].attrs;
-  const tempContainer = createTempContainer();
+  const currentStageAttrs = boardLayer.getParent().attrs;
   const croppedStage = new Konva.Stage({
     width: boardCloneAttrs.width + 20,
     height: boardCloneAttrs.height + 20,
   });
-
-  document.body.removeChild(tempContainer);
 
   boardCloneAttrs.x = 10;
   boardCloneAttrs.y = 10;
@@ -33,6 +20,7 @@ export function getCroppedStage(boardLayer) {
   console.log('two')
   const boardClone = boardLayer.clone();
   const boardCloneAttrs = boardClone.get('.boardGroup')[0].attrs;
+  const currentStageAttrs = boardLayer.getParent().attrs;
   const croppedStage = new Konva.Stage({
     width: boardCloneAttrs.width + 20,
     height: boardCloneAttrs.height + 20,
@@ -50,16 +38,14 @@ export function getCroppedStage(boardLayer) {
 
 export default function generateThumbnail(boardLayer) {
   console.log('three')
-  // const croppedStage = cropStage(boardLayer);
-
   const boardClone = boardLayer.clone();
   const boardCloneAttrs = boardClone.get('.boardGroup')[0].attrs;
+  const currentStageAttrs = boardLayer.getParent().attrs;
   const croppedStage = new Konva.Stage({
     width: boardCloneAttrs.width + 20,
     height: boardCloneAttrs.height + 20,
     container: 'container',
   });
-
 
   boardCloneAttrs.x = 10;
   boardCloneAttrs.y = 10;
